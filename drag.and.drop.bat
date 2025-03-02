cf7afde2b26065095fbb2efc0baecfb8e1745ac5
Sorrow446 <47045733+Sorrow446@users.noreply.github.com>
2025-03-02 13:55:01 +0000

    Add files via upload

---
+++ drag.and.drop.bat
@@ -0,0 +1,21 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if "%~1" == "" (
+    echo Drag and drop a Game Pass save folder onto this batch file.
+    pause >nul
+    exit
+)
+
+cd /d "%~dp0"
+set "cmd=gpts.exe --in-path "!%~1!""
+%cmd%
+
+if %errorlevel% neq 0 (
+    echo Not OK. Press enter to exit.
+    pause >nul
+    exit
+)
+
+echo OK. Press enter to exit.
+pause >nul
